019955e68960578e8a9fb28ef26b27233ffc7c06
Sira Satheesh <satheesh2254@gmail.com>
2024-10-31 13:49:05 +0530

    files are uploaded

---
+++ javascript/day7_control_statements/control_statements.js
@@ -0,0 +1,69 @@
+// 1.print 1 to 3 using for loop
+
+for(var i=1;i<=3;i++)
+{
+    console.log(i)
+}
+
+
+// 2.print 1 to 7 using for loop
+
+for(var i=1;i<=7;i++)
+{
+    console.log(i)
+}
+
+// 3.print 6 to 10 using for loop
+
+for(var i=6;i<=10;i++)
+{
+    console.log(i)
+}
+
+// 4.print 11 to 7 in decrease order using for loop
+
+for(var i=11;i>6;i--)
+{
+    console.log(i)
+}
+
+//while loop
+
+//1.print 1 to 7 using while loop
+
+var i=1
+while(i<=7)
+{
+    console.log(i)
+    i++
+}
+
+// 2.print 10 to 6 in decrease order using while loop
+
+var i=10
+while(i>=6)
+{
+    console.log(i)
+    i--
+}
+
+
+//do while loop
+
+//1.print 1 to 5 using do while loop
+
+var i=1
+
+do{
+    console.log(i)
+    i++
+}while(i<=5)
+
+
+// 2.print 9 to 6 using do while loop
+
+var i=10
+do{
+    i--
+    console.log(i)
+}while(i>=7)
