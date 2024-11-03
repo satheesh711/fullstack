23cf51f89c920d1eede83f46b1969c68adfdbcb1
Sira Satheesh <satheesh2254@gmail.com>
2024-11-03 13:59:01 +0530

    files are uploaded

---
+++ javascript/day10_functions/functions.js
@@ -0,0 +1,15 @@
+
+// Task : create 2 functions with studentName and anotherFunction with their surname and execute the code and write the execution of function behind the scenes  and how FEC creates in call stack and how it vanishes there
+
+
+function studentName(){
+    console.log("satheesh")
+}
+
+function surname(){
+    console.log("sira")
+}
+
+surname()
+studentName()
+console.log("end the exicution")
--- javascript/pratice/pratice.js
+++ javascript/pratice/pratice.js
@@ -1,10 +1,7 @@
-var age = 20;
-if (age >= 18) {
-  if (age >= 21) {
-    console.log("Eligible for driving and drinking.");
-  } else {
-    console.log("Eligible for driving only.");
-  }
-} else {
-  console.log("Not eligible for driving or drinking.");
+let a=10
+{
+  console.log(a)
+  a=20
+  let b=10
 }
+c=0
