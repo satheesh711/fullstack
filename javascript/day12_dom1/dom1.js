718a7604c3ef8057b9524fd28d61e36977b8a0bf
Sira Satheesh <satheesh2254@gmail.com>
2024-11-07 11:22:05 +0530

    files are uploaded

--- javascript/day12_dom1/dom1.js
+++ javascript/day12_dom1/dom1.js
@@ -1,2 +1,9 @@
-let t=document.getElementsByTagName('p')
-t[0].innerHTML="how are you"
+// 1. click a button and show the content with 60px fontSize and bgColor red and bordeRadius 50% 
+let a=document.getElementById("btn")
+let b=document.getElementsByTagName("p")
+console.log(b)
+a.onclick=function (){
+    b[0].style.fontSize="60px"
+    b[0].style.backgroundColor = "red"
+    b[0].style.borderRadius="50%"
+}
